13a81fe377e9527445dd1b8d5df22795a24a6dd8
Mohamed Wassim Hamra <wassimhamraa@gmail.com>
2023-09-10 14:37:29 +0100

    final

--- Django/settings.py
+++ Django/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     BASE_DIR / STATIC_URL
